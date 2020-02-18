22a3d65b28ad885accff9ef17709096939acbbf5
sull <syhwan88@naver.com>
2020-02-18 15:08:43 +0900

    Updates

---
+++ precache-manifest.3c1a707c00003273b3821d66374c31b0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceded51e4f2157c4bc04eea40dc4ae03",
+    "url": "maintainker.github.io/port2/index.html"
+  },
+  {
+    "revision": "f5dce85091310425356c",
+    "url": "maintainker.github.io/port2/static/css/main.b50e0e74.chunk.css"
+  },
+  {
+    "revision": "502119a214f8a62bf2d8",
+    "url": "maintainker.github.io/port2/static/js/2.6a8d4355.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "maintainker.github.io/port2/static/js/2.6a8d4355.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5dce85091310425356c",
+    "url": "maintainker.github.io/port2/static/js/main.947047b1.chunk.js"
+  },
+  {
+    "revision": "a6e2244916785c3a66c3",
+    "url": "maintainker.github.io/port2/static/js/runtime-main.890560c5.js"
+  },
+  {
+    "revision": "11f18a77b66bc41dea18b5a21ebe7c19",
+    "url": "maintainker.github.io/port2/static/media/jung.11f18a77.jpg"
+  },
+  {
+    "revision": "22c5709974aff7a685c6741a6cb94eac",
+    "url": "maintainker.github.io/port2/static/media/mockup3-247.22c57099.png"
+  },
+  {
+    "revision": "b9630ccfdc244defd720b27354248409",
+    "url": "maintainker.github.io/port2/static/media/mockup3.b9630ccf.jpg"
+  },
+  {
+    "revision": "cc11904c45dca3dcf7da4460356a119c",
+    "url": "maintainker.github.io/port2/static/media/standard1.cc11904c.png"
+  }
+]);
